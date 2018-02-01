820415ffda16e4bcc0fda8735ce4dcda9e8158fc
Tung Thanh Le <ttungl@users.noreply.github.com>
2018-01-31 22:28:46 -0600

    Add files via upload

--- source-code/Missing Number 268.py
+++ source-code/Missing Number 268.py
@@ -17,19 +17,33 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # sol 1: 
-        # runtime: 70ms
-        nums = sorted(nums)
+        # sol 1
+        # runtime 77ms
+        nums.sort()
         for i in range(len(nums)):
-            if nums[i] - i != 0:
+            if nums[i] - i != 0: # if value != its index: return index.
                 return i
         return len(nums)
+        
+        # sol 2:
+        # binary search
+        # time O(log n) space O(1)
+        # runtime: 72ms
+        nums.sort() # in-place sort.
+        lo, hi = 0, len(nums)
+        while lo < hi:
+            mid = lo + (hi - lo)/2
+            if nums[mid] > mid: hi = mid
+            else: lo = mid + 1
+        return lo
     
-        # sol 2: XOR
-        # runtime: 55ms 
+        # sol 3: 
+        # xor
+        # time O(n) space O(1)
+        # runtime: 53ms
         n = len(nums)
-        for i in range(n):
-            n = n^i
-            n = n^nums[i]
-        return n
+        for i in range(len(nums)): # i,n=(0,3);(1,3);(2,3). 
+            n = n^i 			   # n=3=3^0; 1=0^1; 3=1^2.
+            n = n^nums[i] 		   # n=0=3^3; 1=1^0; 2=3^1.	
+        return n # 2
 
