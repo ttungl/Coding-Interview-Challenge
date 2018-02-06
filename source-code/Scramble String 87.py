5e4c4f1f874ce9753c6b67074fdd6eed16dce38a
Tung Thanh Le <ttungl@users.noreply.github.com>
2018-02-05 21:19:41 -0600

    Add files via upload

--- source-code/Scramble String 87.py
+++ source-code/Scramble String 87.py
@@ -27,16 +27,13 @@ class Solution(object):
                 if (dfs(s1[:i], s2[:i]) and dfs(s1[-(n-i):], s2[-(n-i):])) or (dfs(s1[:i], s2[-i:]) and dfs(s1[-(n-i):], s2[:(n-i)])):
                     return True
             return False
-
         return dfs(s1, s2)
 
         # sol 2
         # time O(n^2) space O(n)
         # runtime 63ms
         if s1==s2: return True
-        
         if sorted(s1) !=sorted(s2): return False
-        
         for i in range(1, len(s1)):
             if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) or (self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-i])): 
             	return True
