0aaa5dbe43054820f6ecf1b338c4f5abc48e9f94
Tung Thanh Le <ttungl@users.noreply.github.com>
2018-03-11 18:39:30 -0500

    Add files via upload

---
+++ source-code/All Paths From Source to Target 797.py
@@ -0,0 +1,56 @@
+# 797. All Paths From Source to Target
+
+
+# Given a directed, acyclic graph of N nodes.  Find all possible paths from node 0 to node N-1, and return them in any order.
+
+# The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.
+
+# Example:
+# Input: [[1,2], [3], [3], []] 
+# Output: [[0,1,3],[0,2,3]] 
+# Explanation: The graph looks like this:
+# 0--->1
+# |    |
+# v    v
+# 2--->3
+# There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+# Note:
+
+# The number of nodes in the graph will be in the range [2, 15].
+# You can print different paths in any order, but you should keep the order of nodes inside one path.
+
+class Solution(object):
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        # sol 1:
+        # DFS: time O(n) space O(n)
+        # runtime: 336ms
+        def dfs(idx, path):
+            if idx == len(graph)-1:
+                res.append(path)
+            for i in graph[idx]:
+                dfs(i, path + [i])
+        res = []
+        dfs(0, [0])
+        return res
+        
+        # sol 2:
+        # iterative 
+        # runtime: 341ms
+        res, path, lenGraph = [], [0], len(graph)
+        stack = [(0, [0])]
+        while stack:
+            idx, path = stack.pop()
+            if idx == (lenGraph - 1):
+                res.append(path)
+            for i in graph[idx]:
+                stack.append((i,path+[i]))
+        return res
+
+
+
+        
+
---
+++ source-code/Rotate String 796.py
@@ -0,0 +1,47 @@
+# 796. Rotate String
+
+
+# We are given two strings, A and B.
+
+# A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+
+# Example 1:
+# Input: A = 'abcde', B = 'cdeab'
+# Output: true
+
+# Example 2:
+# Input: A = 'abcde', B = 'abced'
+# Output: false
+# Note:
+
+# A and B will have length at most 100.
+
+
+class Solution(object):
+    def rotateString(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+        # sol 1:
+        # runtime: 38ms
+        if not A or not B:
+            return False
+        if A == B:
+            return True
+        if len(A)!=len(B):
+            return False
+        
+        for i in range(len(A)):
+            if (A[i:]+A[0:i]) == B:
+                return True
+        return False
+
+
+        # sol 2:
+        # runtime: 34ms
+        return len(A) == len(B) and B in (A + A)
+
+
+        
