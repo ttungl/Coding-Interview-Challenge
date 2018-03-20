90fd74fbc0a233e0e36660bc3ed2cddaecca6b54
Tung Thanh Le <ttungl@users.noreply.github.com>
2018-03-20 15:31:30 -0500

    Add files via upload

---
+++ source-code/Find Eventual Safe States 802.py
@@ -0,0 +1,96 @@
+# 802. Find Eventual Safe States
+
+
+# In a directed graph, we start at some node and every turn, walk along a directed edge of the graph.  If we reach a node that is terminal (that is, it has no outgoing directed edges), we stop.
+
+# Now, say our starting node is eventually safe if and only if we must eventually walk to a terminal node.  More specifically, there exists a natural number K so that for any choice of where to walk, we must have stopped at a terminal node in less than K steps.
+
+# Which nodes are eventually safe?  Return them as an array in sorted order.
+
+# The directed graph has N nodes with labels 0, 1, ..., N-1, where N is the length of graph.  The graph is given in the following form: graph[i] is a list of labels j such that (i, j) is a directed edge of the graph.
+
+# Example:
+# Input: graph = [[1,2],[2,3],[5],[0],[5],[],[]]
+# Output: [2,4,5,6]
+# Here is a diagram of the above graph.
+
+# Illustration of graph
+
+# Note:
+
+# graph will have length at most 10000.
+# The number of edges in the graph will not exceed 32000.
+# Each graph[i] will be a sorted list of different integers, chosen within the range [0, graph.length - 1].
+
+
+class Solution(object):
+    def eventualSafeNodes(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[int]
+        """
+        # sol 1:
+        # use DFS
+        # runtime: 292ms
+        # states:
+        # 0: not visited yet
+        # 1: safe (walk to terminal)
+        # 2: unsafe (visited, to a cycle)
+        def dfs(graph, cur, states):
+            if states[cur] != 0:
+                return states[cur]==1
+            states[cur] = 2 # visited (unsafe)
+            for j in graph[cur]: # check cycle
+                if not dfs(graph, j, states):
+                    return False
+            states[cur] = 1 # eventually safe.
+            return True
+        #
+        res = []
+        states = [0]*len(graph)
+        for i in range(len(graph)):
+            if dfs(graph, i, states):
+                res.append(i)
+        return res
+        
+        # sol 2:
+        # find nodes that don't lead to a cycle in any paths.
+        # use BFS
+        # runtime: 476ms
+        res, queue = [], []
+        outdict = collections.defaultdict(int)
+        indict = collections.defaultdict(list)
+        for i in range(len(graph)):
+            outdict[i] = len(graph[i])
+            if outdict[i] == 0:
+                queue.append(i)
+            [indict[j].append(i) for j in graph[i]]
+        #
+        while queue:
+            tmp = queue.pop(0) 
+            res.append(tmp)
+            for j in indict[tmp]:
+                outdict[j] -= 1
+                if outdict[j] == 0:
+                    queue.append(j)
+        return sorted(res)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+
+
+
+
+
---
+++ source-code/Minimum Swaps To Make Sequences Increasing 801.py
@@ -0,0 +1,68 @@
+# 801. Minimum Swaps To Make Sequences Increasing
+
+
+# We have two integer sequences A and B of the same non-zero length.
+
+# We are allowed to swap elements A[i] and B[i].  Note that both elements are in the same index position in their respective sequences.
+
+# At the end of some number of swaps, A and B are both strictly increasing.  (A sequence is strictly increasing if and only if A[0] < A[1] < A[2] < ... < A[A.length - 1].)
+
+# Given A and B, return the minimum number of swaps to make both sequences strictly increasing.  It is guaranteed that the given input always makes it possible.
+
+# Example:
+# Input: A = [1,3,5,4], B = [1,2,3,7]
+# Output: 1
+# Explanation: 
+# Swap A[3] and B[3].  Then the sequences are:
+# A = [1, 3, 5, 7] and B = [1, 2, 3, 4]
+# which are both strictly increasing.
+# Note:
+
+# A, B are arrays with the same length, and that length will be in the range [1, 1000].
+# A[i], B[i] are integer values in the range [0, 2000].
+
+
+
+class Solution(object):
+    def minSwap(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: int
+        """
+        # sol 1:
+        # runtime: 62ms
+        n = len(A)
+        keep, swap = ['inf'] * n, ['inf'] * n
+        keep[0], swap[0] = 0, 1
+        for i in range(1, n):
+            if A[i] > A[i-1] and B[i] > B[i-1]:
+                keep[i] = min(keep[i-1], keep[i])
+                swap[i] = min(swap[i-1] + 1, swap[i])
+            if A[i] > B[i-1] and B[i] > A[i-1]:
+                keep[i] = min(keep[i], swap[i-1])
+                swap[i] = min(keep[i-1] + 1, swap[i])
+        return min(swap[-1],keep[-1])
+    
+        # sol 2:
+        # DP-1D
+        # runtime: 57ms
+        n = len(A)
+        keep, swap = ['inf']*n, ['inf']*n
+        keep[0], swap[0] = 0, 1
+        for i in range(1, n):
+            if A[i] > A[i-1] and B[i] > B[i-1]:
+                keep[i] = keep[i-1]
+                swap[i] = swap[i-1] + 1
+            if A[i] > B[i-1] and B[i] > A[i-1]:
+                keep[i] = min(keep[i], swap[i-1])
+                swap[i] = min(swap[i], keep[i-1] + 1)
+        return min(swap[-1], keep[-1])
+        
+        
+        
+
+
+
+
+
